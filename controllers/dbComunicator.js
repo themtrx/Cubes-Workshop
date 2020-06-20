e80b570a62136962400190055e6c964c768367e8
unknown <themtrx@mail.bg>
2020-06-20 18:18:16 +0300

    redner cubes dynamically

---
+++ controllers/dbComunicator.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
+const dbFile = path.join(__dirname, '..', 'config/database.json');
+
+const saveCube = (cube) => {
+
+    getCubes((allCubes) => {
+        allCubes.push(cube);
+
+        fs.writeFile(dbFile, JSON.stringify(allCubes), (error) => {
+            if(error){
+                throw error;
+            }
+        })
+    });
+}
+
+const getOneCube = (id, callback) => {
+    getCubes(cubes => {
+        const cube = cubes.filter(c => c.id === id)[0]
+        callback(cube)
+    })
+}
+
+const getCubes = (callback) => {
+    fs.readFile(dbFile, (error, cubesDB) => {
+
+        if (error){
+            throw error;
+        }
+        
+        const allCubes = JSON.parse(cubesDB);
+        callback(allCubes);
+    });
+}
+
+module.exports = {
+    saveCube,
+    getOneCube,
+    getCubes
+}
